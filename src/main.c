a0fe5edbcb08eb026d2d40a85cc6c8f60b6bf2e4
HeberFHLemes <hfhlemes@gmail.com>
2025-07-21 17:44:41 -0300

    debug: mensagens de log no decorrer da aplicação

--- src/main.c
+++ src/main.c
@@ -7,19 +7,28 @@
 int criarTabelas(sqlite3 *db);
 
 int main(){
-    sqlite3 *db = NULL;
 
+    printf("Iniciando programa...\n");
+    printf("Tentando conectar ao banco de dados...\n");
+
+    sqlite3 *db = NULL;
     if (conectarBD(&db, "minhaestante.db") != SQLITE_OK){
         return 1;
     }
 
     if (criarTabelas(db) != 0){
-        printf("Ocorreu um erro ao criar as tabelas...");
+        printf("Ocorreu um erro ao criar as tabelas para o banco de dados...\n");
+    } else {
+        printf("Banco de dados pronto!\n");
     }
-    
+
+    printf("Pressione ENTER para sair...\n");
+    getchar();
+    printf("Desconectando...\n");
     if (desconectarBD(db) != SQLITE_OK){
         return 1;
     }
+    printf("Desconectado com sucesso, até a próxima!\n");
 
     return 0;
 }

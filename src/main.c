38663286c765204b52c0aaf5a9352c5547035064
HeberFHLemes <hfhlemes@gmail.com>
2025-07-22 13:18:17 -0300

    feat: implementação das funções de atualizar registros de pessoas na main.c

--- src/main.c
+++ src/main.c
@@ -7,16 +7,30 @@
 
 #define SAIR 0
 
+// TODO: 
+// mover funções de operações e fluxo de escolhas para arquivos próprios
+
+// Interface / Fluxo de escolhas do usuário
 void mensagem_inicial();
 int menu_opcoes(sqlite3 *db);
 void direcionar_usuario(sqlite3 *db, int escolha);
 
-void consultar_estante(sqlite3 *db);
+// Operações com BD
 void cadastrar_pessoa(sqlite3 *db);
+void cadastrar_livro(sqlite3 *db);
+void consultar_pessoa(sqlite3 *db);
+void consultar_estante(sqlite3 *db);
+void atualizar_pessoa(sqlite3 *db);
+void atualizar_nome_pessoa(sqlite3 *db);
+void atualizar_telefone_pessoa(sqlite3 *db);
+void remover_registro_pessoa(sqlite3 *db);
+
+// Inputs
 void get_string(char* buffer, size_t tamanho_max, const char* frase);
 int get_escolha_int(const char* frase);
 void limpar_stdin();
 
+// Conexão
 int desconectar(sqlite3 *db);
 
 int main(){
@@ -57,12 +71,8 @@ int desconectar(sqlite3 *db){
     return rc;
 }
 
-void consultar_estante(sqlite3 *db){
-    int pessoa_id = get_escolha_int("Digite o id do dono da estante a ser consultada: ");
-    listar_estante(db, pessoa_id);
-}
-
 void cadastrar_pessoa(sqlite3 *db){
+    printf(" === Cadastro de pessoa ===\n");
     
     char nome[MAX_NOME];
     get_string(nome, MAX_NOME, "Digite o nome: ");
@@ -73,24 +83,120 @@ void cadastrar_pessoa(sqlite3 *db){
     inserir_pessoa(db, nome, telefone);
 }
 
+void cadastrar_livro(sqlite3 *db){
+    printf(" === Cadastro de livro ===\n");
+    
+    char titulo[MAX_NOME];
+    get_string(titulo, MAX_NOME, "Digite o titulo do livro: ");
+    
+    char autor[MAX_NOME];
+    get_string(autor, MAX_NOME, "Digite o nome do autor do livro: ");
+
+    int ano = get_escolha_int("Digite o ano de publicação do livro: ");
+
+    int edicao = get_escolha_int("Digite o número da edição: ");
+
+    int dono_id = get_escolha_int("Digite o id do dono do livro: ");
+
+    inserir_livro(db, dono_id, titulo, autor, ano, edicao);
+}
+
+/* Atualmente chamando listar_pessoas(sqlite3 *db) direto
+void listar_tabela_pessoas(sqlite3 *db){
+    listar_pessoas(db);
+}*/
+
+void consultar_pessoa(sqlite3 *db){
+    int pessoa_id = get_escolha_int("Digite o id da pessoa a ser consultada: ");
+    buscar_pessoa(db, pessoa_id);
+}
+
+void consultar_estante(sqlite3 *db){
+    int pessoa_id = get_escolha_int("Digite o id do dono da estante a ser consultada: ");
+    listar_estante(db, pessoa_id);
+}
+
+void atualizar_pessoa(sqlite3 *db){
+    printf("+--------------------------+\n");
+    printf("|     Atualizar Pessoa     |\n");
+    printf("+--------------------------+\n");
+    printf("| [1] - Alterar nome       |\n");
+    printf("| [2] - Alterar telefone   |\n");
+    printf("| [0] - Voltar             |\n");
+    printf("+--------------------------+\n");
+    int escolha = get_escolha_int("Digite sua escolha (0-2): ");
+
+    switch (escolha){
+        case 1:
+            atualizar_nome_pessoa(db);
+            break;
+        case 2:
+            atualizar_telefone_pessoa(db);
+        default:
+            printf("Opção inválida, tente novamente.\n");
+            return atualizar_pessoa(db);
+    }
+}
+
+void atualizar_nome_pessoa(sqlite3 *db){
+
+    printf(" === Atualizar nome ===\n");
+
+    int id = get_escolha_int("Digite o id da pessoa (ou 0 para cancelar): ");
+    if (id == 0){
+        printf("Cancelando operação...\n");
+        return;
+    }
+
+    char nome[MAX_NOME];
+    get_string(nome, MAX_NOME, "Digite o nome: ");
+    
+    atualizar_nome(db, id, nome);
+}
+
+void atualizar_telefone_pessoa(sqlite3 *db){
+    
+    printf(" === Atualizar telefone ===\n");
+
+    int id = get_escolha_int("Digite o id da pessoa (ou 0 para cancelar): ");
+    if (id == 0){
+        printf("Cancelando operação...\n");
+        return;
+    }
+
+    char telefone[MAX_NOME];
+    get_string(telefone, MAX_NOME, "Digite o telefone para contato: ");
+    
+    atualizar_telefone(db, id, telefone);
+}
+
+void remover_registro_pessoa(sqlite3 *db){
+    return;
+}
+
+
 void direcionar_usuario(sqlite3 *db, int escolha){
     switch (escolha){
         case 1:
             cadastrar_pessoa(db);
             break;
         case 2:
-            return;
+            cadastrar_livro(db);
+            break;
         case 3:
-            return;
+            listar_pessoas(db);
+            break;
         case 4:
-            consultar_estante(db);
+            consultar_pessoa(db);
             break;
         case 5:
-            return;
+            consultar_estante(db);
+            break;
         case 6:
-            return;
+            atualizar_info_pessoa(db);
+            break;
         case 7:
-            return;
+            remover_registro_pessoa(db);
         case 8:
             return;
         default:
@@ -107,12 +213,6 @@ void get_string(char* buffer, size_t tamanho_max, const char* frase){
     if (len > 0 && buffer[len - 1] == '\n') {
         buffer[len - 1] = '\0';
     }
-}
-
-// Limpa o stdin após scanf ou fgets
-void limpar_stdin(){
-    int c;
-    while ((c = getchar()) != '\n' && c != EOF); 
 }
 
 // Apresenta uma frase ao usuário e retorna um int digitado pelo mesmo
@@ -133,6 +233,12 @@ int get_escolha_int(const char* frase){
     return escolha;
 }
 
+// Limpa o stdin após scanf ou fgets
+void limpar_stdin(){
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF); 
+}
+
 int menu_opcoes(sqlite3 *db){
 
     printf("+--------------------------+\n");
@@ -140,28 +246,30 @@ int menu_opcoes(sqlite3 *db){
     printf("+--------------------------+\n");
     printf("| [1] - Cadastrar pessoa   |\n");
     printf("| [2] - Cadastrar livro    |\n");
-    printf("| [3] - Consultar pessoa   |\n");
-    printf("| [4] - Consultar estante  |\n");
-    printf("| [5] - Atualizar pessoa   |\n");
-    printf("| [6] - Remover pessoa     |\n");
-    printf("| [7] - Atualizar livro    |\n");
-    printf("| [8] - Remover livro      |\n");
+    printf("| [3] - Listar pessoas     |\n");
+    printf("| [4] - Consultar pessoa   |\n");
+    printf("| [5] - Consultar estante  |\n");
+    printf("| [6] - Atualizar pessoa   |\n");
+    printf("| [7] - Remover pessoa     |\n");
+    printf("| [8] - Atualizar livro    |\n");
+    printf("| [9] - Remover livro      |\n");
     printf("| [0] - Sair               |\n");
     printf("+--------------------------+\n");
-    return get_escolha_int("Digite sua escolha (0-8): ");
+    return get_escolha_int("Digite sua escolha (0-9): ");
 }
 
 void mensagem_inicial(){
 
-    printf("+============================================================================================================================+\n");
-    printf("||                                                                                                                          ||\n");
-    printf("||    |\\        /| | |\\    | |     |     /\\         ______   _____  _________     /\\     |\\    |  _________   ______        ||\n");
-    printf("||    | \\      / | | | \\   | |     |    /  \\       |        |           |        /  \\    | \\   |      |      |              ||\n");
-    printf("||    |  \\    /  | | |  \\  | |=====|   /----\\      |______  |_____      |       /----\\   |  \\  |      |      |______        ||\n");
-    printf("||    |   \\  /   | | |   \\ | |     |  /      \\     |              |     |      /      \\  |   \\ |      |      |              ||\n");
-    printf("||    |    \\/    | | |    \\| |     | /        \\    |______  ______|     |     /        \\ |    \\|      |      |______        ||\n");
-    printf("||__________________________________________________________________________________________________________________________||\n");
-    printf("\\____________________________________________________________________________________________________________________________/\n");
+    printf("+==========================================================================================================================+\n");
+    printf("||                                                                                                                        ||\n");
+    printf("||    |\\        /| | |\\    | |     |     /\\         ______   _____  _________     /\\     |\\    |  _________   ______      ||\n");
+    printf("||    | \\      / | | | \\   | |     |    /  \\       |        |           |        /  \\    | \\   |      |      |            ||\n");
+    printf("||    |  \\    /  | | |  \\  | |=====|   /----\\      |______  |_____      |       /----\\   |  \\  |      |      |______      ||\n");
+    printf("||    |   \\  /   | | |   \\ | |     |  /      \\     |              |     |      /      \\  |   \\ |      |      |            ||\n");
+    printf("||    |    \\/    | | |    \\| |     | /        \\    |______  ______|     |     /        \\ |    \\|      |      |______      ||\n");
+    printf("||________________________________________________________________________________________________________________________||\n");
+    printf("\\_________________________________________________________________________________________________________________________/\n");
+    printf("%120s\n", "Author: @HeberFHLemes");
 
     printf("Iniciando programa...\n");
 }
